9a3f2915f4d3a3dd729fcfdf4588a43ac8486d54
Deepti <deeptiayyar@gmail.com>
2026-01-20 11:20:46 +0000

    added basic js form handling and typewriter plugin.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Twinkle twinkle little stars,",
+    autoStart: true,
+    delay: 40,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
